65b333c40c78fc68e3092abc947e5f6575d44a10
Dilshodbek <dilshodjongulomov53@gmail.com>
2025-05-07 17:10:03 +0500

    Footer component

--- src/layout/footer/footer.tsx
+++ src/layout/footer/footer.tsx
@@ -1,7 +1,18 @@
 import { FooterProps } from './footer.props'
+import styles from './footer.module.css'
+import cn from 'classnames'
+import { format } from 'date-fns'
 
-const Footer = ({ ...props }: FooterProps): JSX.Element => {
-	return <div {...props}>Footer</div>
+const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+	return (
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div>
+				Sammi © 2022 - {format(new Date(), 'yyyy')}. All rights reserved.
+			</div>
+			<a href='#'>Terms of use</a>
+			<a href='#'>Privacy policy</a>
+		</footer>
+	)
 }
 
 export default Footer
