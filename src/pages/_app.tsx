8a3a3b4fb96300078ee4a5d5215c27a269621c1d
Dilshodbek <dilshodjongulomov53@gmail.com>
2025-05-11 12:19:19 +0500

    NProgress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,28 @@
 import { AppProps } from 'next/app'
 import '../styles/global.css'
+import 'nprogress/nprogress.css'
+import NProgress from 'nprogress'
+import { useEffect } from 'react'
+import { Router } from 'next/router'
 
+NProgress.configure({
+	showSpinner: false,
+})
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+	useEffect(() => {
+		const handleRouteStart = () => NProgress.start()
+		const handleRouteDone = () => NProgress.done()
+
+		Router.events.on('routeChangeStart', handleRouteStart)
+		Router.events.on('routeChangeComplete', handleRouteDone)
+		Router.events.on('routeChangeError', handleRouteDone)
+
+		return () => {
+			Router.events.off('routeChangeStart', handleRouteStart)
+			Router.events.off('routeChangeComplete', handleRouteDone)
+			Router.events.off('routeChangeError', handleRouteDone)
+		}
+	}, [])
 	return <Component {...pageProps} />
 }
 
